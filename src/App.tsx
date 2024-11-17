8d4088ff482cb59719cdcf27bfa9cd651f7c6410
Chris Lane Jones <chrislanejones@gmail.com>
2024-11-16 23:58:34 -0500

    Created Four Colored Square Floors

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,22 @@ function App() {
         <pointLight position={[-60, 10, 10]} intensity={0.2} />
         <pointLight position={[-15, 10, 10]} intensity={0.3} />
         <pointLight position={[50, 10, 10]} intensity={0.3} />
+        <mesh position={[0, -0.01, 0]} rotation={[0, 0, 0]}>
+          <boxGeometry args={[0.05, 0.01, 0.05]} />
+          <meshStandardMaterial color="#ccc" />
+        </mesh>
+        <mesh position={[0.05, -0.01, 0]} rotation={[0, 0, 0]}>
+          <boxGeometry args={[0.05, 0.01, 0.05]} />
+          <meshStandardMaterial color="#000" />
+        </mesh>
+        <mesh position={[0.05, -0.01, 0.05]} rotation={[0, 0, 0]}>
+          <boxGeometry args={[0.05, 0.01, 0.05]} />
+          <meshStandardMaterial color="#ccc" />
+        </mesh>
+        <mesh position={[0, -0.01, 0.05]} rotation={[0, 0, 0]}>
+          <boxGeometry args={[0.05, 0.01, 0.05]} />
+          <meshStandardMaterial color="#000" />
+        </mesh>
         <Pawn />
       </Canvas>
     </>
