835fa0fdb9b60b02ebe426d9b9c8c681bc34ab21
Chris Lane Jones <chrislanejones@gmail.com>
2024-11-22 23:31:30 -0500

    Testing Rook on Cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Canvas } from "@react-three/fiber";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Instances, Rook } from "./components/rook";
 import { Chessboard } from "./components/chessboard";
 import DraggableObject from "./components/draggable";
 
 const App: FC = () => {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <>
       <Canvas
-        style={{ background: "#6a6a6a" }}
+        style={{ background: "#6a6a6a", cursor: hovered ? "pointer" : "auto" }}
         camera={{ position: [7, 5, 0], fov: 60 }}
       >
         <ambientLight intensity={5} />
@@ -16,7 +18,12 @@ const App: FC = () => {
         <DraggableObject position={[2.6, 0.3, 2.5]} />
 
         <Instances>
-          <Rook scale={25} position={[3.5, 0.04, 3.5]} />
+          <Rook
+            scale={25}
+            position={[3.5, 0.04, 3.5]}
+            onPointerOver={() => setHovered(true)}
+            onPointerOut={() => setHovered(false)}
+          />
         </Instances>
         <Chessboard />
       </Canvas>
